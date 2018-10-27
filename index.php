cd9bf71796a1476f3a0a35776c4dd1e246bce299
Michal Franc <mfranc@email.cz>
2018-10-27 16:42:32 +0200

    added favicon

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Dashboard</title>
+    <title>Color Reference</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="/css/main.min.css">
-    <link rel="stylesheet" href="https://code.cdn.mozilla.net/fonts/fira.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
     <script src='/script/main.min.js' async></script>
 </head>
